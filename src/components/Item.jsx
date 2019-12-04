67a98b9517105d1b5da8e1802bb58e4d6fd8898e
andrewcs0901 <andrewcs0901@gmail.com>
2019-12-03 21:25:33 -0300

    deletando exports desnecessarios

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import DelectCompoment from 'react-swipe-to-delete-component';
 
 export default class Item extends Component {
 
@@ -7,9 +6,6 @@ export default class Item extends Component {
 
         return (
             <div>
-                <DelectCompoment key={this.props.id} value={this.props.id}>
-                    <p className="list-group-item-text">{this.props.text}</p>
-                </DelectCompoment>
             </div>
 
         )
--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import 'Input.css';
+import './Input.css';
 
 export default class Input extends Component {
 
